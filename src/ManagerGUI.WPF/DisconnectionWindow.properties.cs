2de8a085c27d9d8bcb7ecb7bc2783fa644f8f605
Sam Lu <549429518@qq.com>
2017-12-08 08:27:52 +0800

    实现DisconnectionWindow在两个状态下的样式。
    1. 实现DisconnectionWindow在两个状态下的样式。

---
+++ src/ManagerGUI.WPF/DisconnectionWindow.properties.cs
@@ -0,0 +1,36 @@
+﻿using SamLu.Native.Wifi;
+using System;
+using System.Windows;
+
+namespace SamLu.Tools.Wlan_edu_Manager.GUI
+{
+    partial class DisconnectionWindow
+    {
+        #region ConnectionState
+        public static readonly DependencyProperty ConnectionStateProperty =
+            DependencyProperty.Register(
+                nameof(ConnectionState), typeof(ConnectionState), typeof(DisconnectionWindow),
+                new FrameworkPropertyMetadata(ConnectionState.Disconnected, FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsParentArrange | FrameworkPropertyMetadataOptions.AffectsParentMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
+                    new PropertyChangedCallback(DisconnectionWindow.ConnectionStatePropertyChangedCallback)
+                ),
+                DisconnectionWindow.ConnectionStateValidateValueCallback
+            );
+
+        private static void ConnectionStatePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+#warning 未实现。
+        }
+
+        private static bool ConnectionStateValidateValueCallback(object value)
+        {
+            return Enum.IsDefined(typeof(ConnectionState), value);
+        }
+
+        public ConnectionState ConnectionState
+        {
+            get => (ConnectionState)this.GetValue(ConnectionStateProperty);
+            set => this.SetValue(ConnectionStateProperty, value);
+        }
+        #endregion
+    }
+}
---
+++ src/wlanedumain/SamLu/Native/Wifi/ConnectionState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamLu.Native.Wifi
+{
+    /// <summary>
+    /// 表示网络连接的状态。
+    /// </summary>
+    public enum ConnectionState
+    {
+        /// <summary>
+        /// 已连接。
+        /// </summary>
+        Connected,
+        /// <summary>
+        /// 未连接。
+        /// </summary>
+        Disconnected
+    }
+}
